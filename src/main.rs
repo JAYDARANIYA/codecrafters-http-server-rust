0cdd3e233b424c015bf7778bde2d8be286125f92
Jay Daraniya <daraniyajay@gmail.com>
2023-11-11 01:59:35 +0530

    stage 7

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,18 @@ use server::{http_response::HttpResponse, http_status::HttpStatus, HttpServer};
 mod server;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // read --directory directory/  argument
+    let args: Vec<String> = std::env::args().collect();
+
+    if args.len() != 3 {
+        println!("Usage: ./server --directory directory/");
+        return Ok(());
+    }
+
+    let directory = args[2].clone();
+
+    println!("Serving directory: {}", directory);
+
     HttpServer::default()
         .get("/", |_| HttpResponse::from_status(HttpStatus::Ok))
         .get("/echo/*", |request| HttpResponse {
@@ -19,6 +31,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
                 Some(user_agent) => user_agent.to_string(),
                 None => "No User-Agent header".to_string(),
             },
+        })
+        .get("/file/*", move |request| {
+            let path = request.path.join("/").replace("/files/", "");
+            let file_path = format!("{}/{}", directory.clone(), path);
+            match std::fs::read_to_string(file_path) {
+                Ok(content) => HttpResponse {
+                    status: HttpStatus::Ok,
+                    headers: {
+                        let mut headers = HashMap::new();
+                        headers.insert(
+                            "Content-Type".to_string(),
+                            "application/octet-stream".to_string(),
+                        );
+                        headers
+                    },
+                    body: content,
+                },
+                Err(_) => HttpResponse::from_status(HttpStatus::NotFound),
+            }
         })
         .run("127.0.0.1", 4221)?;
     Ok(())
